9409978af06740e657393343ba8b26f4e9e63755
HaatoChama1 <86001218+PatrickMeneses@users.noreply.github.com>
2025-05-05 16:13:14 +0800

    Update sdvxcont.ino

--- Sketch/sdvxcont.ino
+++ Sketch/sdvxcont.ino
@@ -8,6 +8,14 @@ int J = 4;
 int K = 5;
 int N = 6;
 int V = 7;
+int outb1 = 16;
+int outa1 = 10;
+int outb2 = 14;
+int outa2 = 15;
+int stoutb1;
+int stoutb2;
+int lstoutb1;
+int lstoutb2;
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,12 +25,23 @@ pinMode(J, INPUT_PULLUP);
 pinMode(K, INPUT_PULLUP);
 pinMode(N, INPUT_PULLUP);
 pinMode(V, INPUT_PULLUP);
+pinMode(outb1, INPUT_PULLUP);
+pinMode(outa1, INPUT_PULLUP);
+pinMode(outb2, INPUT_PULLUP);
+pinMode(outa2, INPUT_PULLUP);
+
 Keyboard.begin();
+
+stoutb2 = digitalRead(outb2);
+stoutb1 = digitalRead(outb1);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+lstoutb1 = digitalRead(outb1);
+lstoutb2 = digitalRead(outb2);
 
+// button logic
 if(digitalRead(D) == LOW ){
 Keyboard.press('d');
 } else{
@@ -53,4 +72,31 @@ Keyboard.press('v');
 }else {
 Keyboard.release('v');
 }
+// knob logic
+if(lstoutb1 != stoutb1 && lstoutb1 == LOW) {
+ if(digitalRead(outa1) != lstoutb1){
+//Keyboard.press();
+ } else if (digitalRead(outa1) == lstoutb1) {
+//Keyboard.press();
+ } else {
+  //Keyboard.release();
+  //Keyboard.release();
+ }
+if(lstoutb2 != stoutb2 && lstoutb2 == LOW) {
+ if(digitalRead(outa2) != lstoutb2) {
+//Keyboard.press();
+ } else if (digitalRead(outa2) == lstoutb2) {
+//Keyboard.press();
+ } else {
+  //Keyboard.release();
+  //Keyboard.release();
+ }
+}
+stoutb1 = lstoutb1;
+stoutb2 = lstoutb2;
+}
+}
+
+stoutb1 = lstoutb1;
+stoutb2 = lstoutb2;
 }
